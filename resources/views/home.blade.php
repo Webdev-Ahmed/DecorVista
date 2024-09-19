505cf385ef6fea0b66ed41689fbbef5a5ec2a34d
unknown <Administrator@LAB1-PC14.Lab.com>
2024-09-19 10:11:33 +0500

    Initiated

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    <title>DecorVista | Interior Designer</title>
+</head>
+<body>
+    <h1 class="text-3xl font-bold">Heloo</h1>
+</body>
+</html>
